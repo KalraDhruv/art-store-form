ae28d121991d896c5b744c922523d7d063ea11e0
KalraDhruv <dhruvkalra@proton.me>
2024-07-08 08:08:43 +0000

    Added navigation functionality

--- header.inc.php
+++ header.inc.php
@@ -1,3 +1,6 @@
+<?php
+include 'data.inc.php';
+?>
 <header class="header">  
   <div class="header__wrapper">
     <h1 class="header__heading">Art Store</h1>
@@ -5,8 +8,12 @@
       <ul class="navigation__list">
         <?php
         /* write loop to generate list items as follows: 
+	 * 
             <li class='navigation__list-item'><a href='index.php'>Home</a></li> */
+	foreach($links as $link){
+			echo "<li class='navigation__list-item'><a href='".$link["url"]."'>".$link["label"]."</a></li>";
 
+	}	
         ?>
       </ul>
     </nav>  
